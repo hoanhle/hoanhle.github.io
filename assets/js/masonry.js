314bea7a13aa0fef6b69ccd0b7ac4b6ac6c7dd3f
Hoanh Le <hoanhle100100@gmail.com>
2023-11-19 18:28:05 +0200

    l-gutter size change

--- assets/js/masonry.js
+++ assets/js/masonry.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   // Init Masonry
   var $grid = $('.grid').masonry({
-    gutter: 15,
+    gutter: 25,
     horizontalOrder: true,
     itemSelector: '.grid-item',
   });
